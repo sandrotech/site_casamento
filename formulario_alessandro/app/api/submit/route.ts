dc897b22902c220f2e30b29c8957effb3567b850
Alessandro Barbosa <alessandro.santos@fagifor.fortaleza.ce.gov.br>
2025-11-14 16:42:30 -0300

    zerando

--- formulario_alessandro/app/api/submit/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { salvarResposta } from "@/lib/db";
-
-export async function POST(req: Request) {
-    const data = await req.json();
-    salvarResposta(data);
-    return NextResponse.json({ success: true });
-}
