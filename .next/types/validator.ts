81b83d99e46ca95146f2d2d8efed31c579f2f6ce
Alessandro Barbosa <alessandro.santos@fagifor.fortaleza.ce.gov.br>
2025-12-29 10:22:29 -0300

    dados insridos

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -119,6 +119,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../app/api/public-images/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/public-images">> = Specific
+  const handler = {} as typeof import("../../app/api/public-images/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../app/api/rsvp/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/rsvp">> = Specific
